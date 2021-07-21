0cc44d86f0f5431f6e4d38389c360226d36c63d0
Shoeboxam <shoeboxam@gmail.com>
2021-07-21 13:17:58 -0400

    ugly generic on CompatiblePairing, drop subdomain checks

--- rust/opendp/src/pairing.rs
+++ rust/opendp/src/pairing.rs
@@ -2,18 +2,28 @@ use crate::dom::{AllDomain, IntervalDomain, InherentNullDomain, InherentNull, Ve
 use crate::core::Domain;
 use crate::dist::{AbsoluteDistance, SymmetricDistance};
 
-pub trait CompatiblePairing {}
-// type parameter T is unconstrained
-impl<T, D: Subdomain<AllDomain<T>>, Q> CompatiblePairing for (D, AbsoluteDistance<Q>) {}
-impl<D: Subdomain<VectorDomain<D>>> CompatiblePairing for (D, SymmetricDistance) {}
+// How do we get rid of I?
+// I is the type that the domain is parameterized by.
+// - It exists for the Subdomain bound
+// - It breaks the trait
+pub trait CompatiblePairing<I> {}
+
+impl<T, D: Subdomain<AllDomain<T>>, Q> CompatiblePairing<T> for (D, AbsoluteDistance<Q>) {}
+impl<DA: Domain, D: Subdomain<VectorDomain<DA>>> CompatiblePairing<DA> for (D, SymmetricDistance) {}
 
 
 pub trait Subdomain<D1: Domain>: Domain {}
 impl<T> Subdomain<AllDomain<T>> for AllDomain<T> {}
 impl<T: PartialOrd + Clone> Subdomain<AllDomain<T>> for IntervalDomain<T> {}
-impl<T, D: Subdomain<AllDomain<T>>> Subdomain<AllDomain<T>> for InherentNullDomain<D>
-    where T: InherentNull {}
-// type parameter T is unconstrained
-impl<T, D: Subdomain<AllDomain<T>>> Subdomain<VectorDomain<D>> for VectorDomain<D> {}
-// type parameter T is unconstrained
-impl<T, D: Subdomain<AllDomain<T>>> Subdomain<VectorDomain<D>> for SizedDomain<VectorDomain<D>> {}
+impl<D: Domain> Subdomain<AllDomain<D::Carrier>> for InherentNullDomain<D>
+    where D::Carrier: InherentNull {}
+impl<D: Domain> Subdomain<VectorDomain<D>> for VectorDomain<D> {}
+impl<D: Domain> Subdomain<VectorDomain<D>> for SizedDomain<VectorDomain<D>> {}
+
+
+// This impl requires that the inner domain of VectorDomain is a Subdomain of AllDomain
+// - It has the same issue that caused the I generic on CompatiblePairing to pop out
+// // type parameter T is unconstrained
+// impl<T, D: Subdomain<AllDomain<T>>> Subdomain<VectorDomain<D>> for VectorDomain<D> {}
+// // type parameter T is unconstrained
+// impl<T, D: Subdomain<AllDomain<T>>> Subdomain<VectorDomain<D>> for SizedDomain<VectorDomain<D>> {}
